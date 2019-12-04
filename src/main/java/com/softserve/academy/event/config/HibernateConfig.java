4568b86e8cd7d92b8b6db5c6d71d11411a1945c4
mvitktc <clevercattv@gmail.com>
2019-12-04 15:11:20 +0200

    template to add transactional, new this didn't work

--- src/main/java/com/softserve/academy/event/config/HibernateConfig.java
+++ src/main/java/com/softserve/academy/event/config/HibernateConfig.java
@@ -1,11 +1,21 @@
 package com.softserve.academy.event.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -13,13 +23,38 @@ import static org.hibernate.cfg.AvailableSettings.*;
 
 @Configuration
 @PropertySource("classpath:database.properties")
+@EnableTransactionManagement
+@Slf4j
 public class HibernateConfig {
 
     private static final String ENTITY_PACKAGE = "hibernate.entity.package";
 
+    private final Environment environment;
+
+    @Autowired
+    public HibernateConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
-    public LocalSessionFactoryBean getSessionFactory(Environment environment) {
+    public DataSource getDataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass(Objects.requireNonNull(environment.getProperty(DRIVER)));
+        } catch (PropertyVetoException e) {
+            log.error(e.getMessage());
+            System.exit(1);
+        }
+        dataSource.setJdbcUrl(Objects.requireNonNull(environment.getProperty(URL)));
+        dataSource.setUser(Objects.requireNonNull(environment.getProperty(USER)));
+        dataSource.setPassword(Objects.requireNonNull(environment.getProperty(PASS)));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(getDataSource());
         Properties properties = new Properties();
 
         properties.put(DRIVER, Objects.requireNonNull(environment.getProperty(DRIVER)));
@@ -30,10 +65,23 @@ public class HibernateConfig {
         properties.put(HBM2DDL_AUTO, Objects.requireNonNull(environment.getProperty(HBM2DDL_AUTO)));
         properties.put(DIALECT, Objects.requireNonNull(environment.getProperty(DIALECT)));
 
+        properties.put(C3P0_MIN_SIZE, Objects.requireNonNull(environment.getProperty(C3P0_MIN_SIZE)));
+        properties.put(C3P0_MAX_SIZE, Objects.requireNonNull(environment.getProperty(C3P0_MAX_SIZE)));
+        properties.put(C3P0_ACQUIRE_INCREMENT, Objects.requireNonNull(environment.getProperty(C3P0_ACQUIRE_INCREMENT)));
+        properties.put(C3P0_TIMEOUT, Objects.requireNonNull(environment.getProperty(C3P0_TIMEOUT)));
+        properties.put(C3P0_MAX_STATEMENTS, Objects.requireNonNull(environment.getProperty(C3P0_MAX_STATEMENTS)));
+
         sessionFactoryBean.setHibernateProperties(properties);
         sessionFactoryBean.setPackagesToScan(environment.getProperty(ENTITY_PACKAGE));
 
         return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
     }
 
 }
