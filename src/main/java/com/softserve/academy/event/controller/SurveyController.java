6b2f457520a0f00923c858e33856af780e8040be
mvitktc <clevercattv@gmail.com>
2019-12-06 18:34:50 +0200

    change argument

--- src/main/java/com/softserve/academy/event/controller/SurveyController.java
+++ src/main/java/com/softserve/academy/event/controller/SurveyController.java
@@ -2,6 +2,7 @@ package com.softserve.academy.event.controller;
 
 import com.softserve.academy.event.annotation.PageableDefault;
 import com.softserve.academy.event.dto.SimpleSurveyDTO;
+import com.softserve.academy.event.entity.Survey;
 import com.softserve.academy.event.response.ServerResponse;
 import com.softserve.academy.event.service.SurveyService;
 import com.softserve.academy.event.util.Page;
@@ -36,9 +37,9 @@ public class SurveyController {
     }
 
     @DeleteMapping
-    public ServerResponse<Long> deleteSurvey(Long id) {
-        service.deleteById(id);
-        return ServerResponse.success(id);
+    public ServerResponse<Long> deleteSurvey(Survey survey) {
+        service.delete(survey);
+        return ServerResponse.success(survey.getId());
     }
 
 }
