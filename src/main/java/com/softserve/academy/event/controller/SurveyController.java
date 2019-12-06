15f957b868e0989eb86137fde7da6c9c70ec804b
mvitktc <clevercattv@gmail.com>
2019-12-06 16:39:36 +0200

    complete survey controller

--- src/main/java/com/softserve/academy/event/controller/SurveyController.java
+++ src/main/java/com/softserve/academy/event/controller/SurveyController.java
@@ -6,9 +6,8 @@ import com.softserve.academy.event.response.ServerResponse;
 import com.softserve.academy.event.service.SurveyService;
 import com.softserve.academy.event.util.Page;
 import com.softserve.academy.event.util.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.softserve.academy.event.util.Sort;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("survey")
@@ -21,8 +20,25 @@ public class SurveyController {
     }
 
     @GetMapping
-    public ServerResponse<Page<SimpleSurveyDTO>> findAllSurveys(@PageableDefault Pageable pageable) {
+    public ServerResponse<Page<SimpleSurveyDTO>> findAllSurveys(
+            @PageableDefault(sort = {"creationDate"}, direction = Sort.Direction.DESC) Pageable pageable) {
         return ServerResponse.success(service.findAll(pageable));
+    }
+
+    @PostMapping
+    public ServerResponse<SimpleSurveyDTO> duplicateSurvey(Long id) {
+        return ServerResponse.success(service.duplicateSurvey(id));
+    }
+
+    @PutMapping
+    public ServerResponse<String> updateTitle(Long id, String title) {
+        return ServerResponse.success(service.setTitleForSurvey(id, title));
+    }
+
+    @DeleteMapping
+    public ServerResponse<Long> deleteSurvey(Long id) {
+        service.deleteById(id);
+        return ServerResponse.success(id);
     }
 
 }
