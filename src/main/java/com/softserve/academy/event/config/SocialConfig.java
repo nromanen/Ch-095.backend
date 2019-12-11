d17f21efa4e8be6251b9dc66a7b05d54f8abaf53
vpeletc <viktorpelpiak@gmail.com>
2019-12-11 19:02:52 +0200

    Base check

--- src/main/java/com/softserve/academy/event/config/SocialConfig.java
+++ src/main/java/com/softserve/academy/event/config/SocialConfig.java
@@ -1,54 +1,54 @@
-package com.softserve.academy.event.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.social.UserIdSource;
-import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
-import org.springframework.social.config.annotation.EnableSocial;
-import org.springframework.social.config.annotation.SocialConfigurer;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
-import org.springframework.social.connect.web.ConnectController;
-import org.springframework.social.facebook.connect.FacebookConnectionFactory;
-import org.springframework.social.security.AuthenticationNameUserIdSource;
-
-import javax.sql.DataSource;
-
-@Configuration
-@EnableSocial
-public class SocialConfig implements SocialConfigurer {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Override
-    public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
-        connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
-                environment.getProperty("facebook.clientId"),
-                environment.getProperty("facebook.clientSecret")));
-    }
-
-    @Override
-    public UserIdSource getUserIdSource() {
-        return new AuthenticationNameUserIdSource();
-    }
-
-    @Override
-    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
-        return new JdbcUsersConnectionRepository(
-                dataSource,
-                connectionFactoryLocator,
-                Encryptors.noOpText()
-        );
-    }
-
-    @Bean
-    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
-        return new ConnectController(connectionFactoryLocator, connectionRepository);
-    }
-}
+//package com.softserve.academy.event.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//import org.springframework.security.crypto.encrypt.Encryptors;
+//import org.springframework.social.UserIdSource;
+//import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
+//import org.springframework.social.config.annotation.EnableSocial;
+//import org.springframework.social.config.annotation.SocialConfigurer;
+//import org.springframework.social.connect.ConnectionFactoryLocator;
+//import org.springframework.social.connect.ConnectionRepository;
+//import org.springframework.social.connect.UsersConnectionRepository;
+//import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+//import org.springframework.social.connect.web.ConnectController;
+//import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+//import org.springframework.social.security.AuthenticationNameUserIdSource;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@EnableSocial
+//public class SocialConfig implements SocialConfigurer {
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Override
+//    public void addConnectionFactories(ConnectionFactoryConfigurer connectionFactoryConfigurer, Environment environment) {
+//        connectionFactoryConfigurer.addConnectionFactory(new FacebookConnectionFactory(
+//                environment.getProperty("facebook.clientId"),
+//                environment.getProperty("facebook.clientSecret")));
+//    }
+//
+//    @Override
+//    public UserIdSource getUserIdSource() {
+//        return new AuthenticationNameUserIdSource();
+//    }
+//
+//    @Override
+//    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+//        return new JdbcUsersConnectionRepository(
+//                dataSource,
+//                connectionFactoryLocator,
+//                Encryptors.noOpText()
+//        );
+//    }
+//
+//    @Bean
+//    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
+//        return new ConnectController(connectionFactoryLocator, connectionRepository);
+//    }
+//}
--- src/main/java/com/softserve/academy/event/controller/FacebookController.java
+++ src/main/java/com/softserve/academy/event/controller/FacebookController.java
@@ -1,42 +1,42 @@
-package com.softserve.academy.event.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.PagedList;
-import org.springframework.social.facebook.api.Post;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/facebook")
-public class FacebookController {
-
-    private Facebook facebook;
-
-    @Autowired
-    public FacebookController(Facebook facebook) {
-        this.facebook = facebook;
-    }
-
-    @GetMapping
-    public String getFacebookFeeds(Model model){
-    //        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
-    //            return "redirect:/connect/facebook";
-    //        }
-    //        PagedList<Post> posts = facebook.feedOperations().getPosts();
-    //        model.addAttribute("profileName", posts.get(0).getFrom().getName());
-    //        model.addAttribute("posts", posts);
-    //        return "profile";
-
-        if (!facebook.isAuthorized()) {
-            return "redirect:/connect/facebook";
-        }
-
-        model.addAttribute(facebook.userOperations().getUserProfile());
-        PagedList homeFeed = facebook.feedOperations().getHomeFeed();
-        model.addAttribute("feed", homeFeed);
-        return "feeds";
-    }
-}
+//package com.softserve.academy.event.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.social.facebook.api.Facebook;
+//import org.springframework.social.facebook.api.PagedList;
+//import org.springframework.social.facebook.api.Post;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/facebook")
+//public class FacebookController {
+//
+//    private Facebook facebook;
+//
+//    @Autowired
+//    public FacebookController(Facebook facebook) {
+//        this.facebook = facebook;
+//    }
+//
+//    @GetMapping
+//    public String getFacebookFeeds(Model model){
+//    //        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+//    //            return "redirect:/connect/facebook";
+//    //        }
+//    //        PagedList<Post> posts = facebook.feedOperations().getPosts();
+//    //        model.addAttribute("profileName", posts.get(0).getFrom().getName());
+//    //        model.addAttribute("posts", posts);
+//    //        return "profile";
+//
+//        if (!facebook.isAuthorized()) {
+//            return "redirect:/connect/facebook";
+//        }
+//
+//        model.addAttribute(facebook.userOperations().getUserProfile());
+//        PagedList homeFeed = facebook.feedOperations().getHomeFeed();
+//        model.addAttribute("feed", homeFeed);
+//        return "feeds";
+//    }
+//}
