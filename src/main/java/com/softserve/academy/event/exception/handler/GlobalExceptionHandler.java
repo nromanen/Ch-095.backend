c5041dcc964f77e03de1a934b6398f6b61b71001
clevercattv <clevercattv@gmail.com>
2019-12-17 22:28:39 +0200

    remove genius sessionFactory logic, change exception handler annotation

--- src/main/java/com/softserve/academy/event/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/academy/event/exception/handler/GlobalExceptionHandler.java
@@ -4,11 +4,11 @@ import com.softserve.academy.event.exception.UserNotFountException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
