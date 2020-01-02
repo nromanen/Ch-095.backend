e372d283c5b24ddbb7864aa04d743e7dfdbddde9
Qwe1999 <vladbanar5@gmail.com>
2020-01-02 16:56:36 +0300

    deleted imports

--- src/test/java/com/softserve/academy/event/controller/StatisticControllerTest.java
+++ src/test/java/com/softserve/academy/event/controller/StatisticControllerTest.java
@@ -25,11 +25,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Stream;
-
-import java.util.Arrays;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
