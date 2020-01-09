625900f547c45be9a61b361fd8dd122e5487a788
mvitktc <clevercattv@gmail.com>
2020-01-09 13:52:59 +0200

    change tests dependencies

--- src/test/java/com/softserve/academy/event/service/db/impl/SurveyServiceImplTest.java
+++ src/test/java/com/softserve/academy/event/service/db/impl/SurveyServiceImplTest.java
@@ -6,13 +6,8 @@ import com.softserve.academy.event.controller.ApplicationInitializer;
 import com.softserve.academy.event.dto.SurveyDTO;
 import com.softserve.academy.event.service.db.SurveyService;
 import com.softserve.academy.event.util.Pageable;
-import com.softserve.academy.event.util.Sort;
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -23,14 +18,13 @@ import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(DataProviderRunner.class)
 @SpringJUnitConfig
 @SpringJUnitWebConfig({ApplicationInitializer.class})
 @WithMockUser(username = "clevercattv@gmail.com", authorities = {"USER"})
 @ContextConfiguration(classes = {HibernateConfig.class, WebConfig.class})
-@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/surveyService.sql")
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/beforeSurveyService.sql")
 class SurveyServiceImplTest {
 
     @Autowired
@@ -41,18 +35,15 @@ class SurveyServiceImplTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    @DataProvider
-    public static Object[][] findAllByPageableAndStatusProvider() {
-        return new Object[][]{
-                {new Pageable(12, 0, 1, Sort.from(Sort.Direction.ASC, "id")), null},
-                {new Pageable(12, 0, 1, Sort.from(Sort.Direction.ASC, "id")), ""},
-                {new Pageable(12, 0, 1, Sort.from(Sort.Direction.ASC, "id")), "ACTIVE"},
-        };
+    @Test
+    public void findAllByPageableAndStatus() {
+        System.out.println(surveyService);
+//        List<SurveyDTO> surveys = surveyService.findAllByPageableAndStatus(pageable, status).getItems();
+//        assertEquals(0, surveys.size());
     }
 
     @Test
-    @UseDataProvider("findAllByPageableAndStatusProvider")
-    public void findAllByPageableAndStatus(Pageable pageable, String status) {
+    public void checkFilterByStatus(Pageable pageable, String status) {
         List<SurveyDTO> surveys = surveyService.findAllByPageableAndStatus(pageable, status).getItems();
         assertEquals(0, surveys.size());
     }
